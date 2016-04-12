f029ee99262838cb306da4bcbc1e5f933dc4a160
ck47ss <mck2775@gmail.com>
2016-04-12 10:58:53 -0600

    SCRIPT JAVASCRIPT

---
+++ javascript/js/codigo.js
@@ -0,0 +1,16 @@
+	var  botonClic=function()
+		{
+		alert("click del boton ");
+			// body...
+		}	
+
+	
+		var nombre="MICHEL";
+
+		document.write("<strong> JS EN ACCION </strong>");+ "" +nombre;
+		alert("MENSAJE POR PANTALLA" );
+		console.log("A punto de lanzar un mensaje ");
+		alert(" MENSAJE POR PANTALLA ");
+
+
+	
