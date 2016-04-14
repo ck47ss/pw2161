11b13a9ed64720587e8aa9ccf143f5eb8be3c2d5
ck47ss <mck2775@gmail.com>
2016-04-14 10:56:01 -0600

    CALCULADORA CON FALLAS

---
+++ javascript/calculadora/js/calculadora.js
@@ -0,0 +1,66 @@
+//variable global.
+var operador = "";
+
+
+function numeros(num)
+{
+	var valor= document.calculadora.operando1.value;
+	if (operador== "" ) //ESCRIBIR EN EL OPERANDO 1
+	{
+		if (valor=="0")	//VACIAMOS LA CAJA
+		 {
+		 	document.calculadora.operando1.value;
+		 	//CONCATENAR LOS VALORES DE NUM CON LOS DEL OPERANDO 1
+		 	document.calculadora.operando1.value =
+		 	document.calculadora.operando1.value+num;
+		 }
+		 else
+		 {
+		var valor= document.calculadora.operando2.value;
+		if (operador== "" ) //ESCRIBIR EN EL OPERANDO 1
+	{
+		if (valor=="0")	//VACIAMOS LA CAJA
+		 {
+		 	document.calculadora.operando2.value;
+		 	//CONCATENAR LOS VALORES DE NUM CON LOS DEL OPERANDO 1
+		 	document.calculadora.operando2.value =
+		 	document.calculadora.operando2.value+num;
+		 }
+	}
+
+}
+
+function igual()
+{
+var valor1 = document.calculadora.operando1.value;
+var valor2= document.calculadora.operando2.value;
+document.calculadora.resultado.value=eval(valor1+operador+valor2)
+/*
+if (OPERANDO=='+') 
+{
+	resultado=operando1+operando2;
+}
+
+if (OPERANDO=='-') 
+{
+	resultado=operando1-operando2;
+}
+
+if (OPERANDO=='*') 
+{
+	resultado=operando1*operando2;
+}
+
+if (OPERANDO=='/') 
+{
+	resultado=operando1/operando2;
+}
+
+*/
+}
+
+function operadores(ope)
+{
+	operador=ope;
+
+}
