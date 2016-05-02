6b0687cda164680de339610329a52c03567dbdb5
ck47ss <mck2775@gmail.com>
2016-05-02 11:02:43 -0600

    TECLAS E INPUT EN JQUERY

--- jquery/js/eventos.js
+++ jquery/js/eventos.js
@@ -1,15 +1,48 @@
 var inicio = function()
 {
+
 	var clicboton= function()
 	 {
 	 	console.log("CLICK DEL BOTON ");
-	 	$(".anuncioweb").html("Click  del boton ");
-	 	$(".anuncioweb").append("Click  del boton ");
+	 	$(".anuncioweb").html("click  del boton ");
+	 	$(".anuncioweb").append("click  del boton ");
 
 	 }
+	 var clicboton2= function()
+	 {
+
+	 	alert("BOTON  2");
+	 }
+
+	 var teclauninput = function(tecla)
+	 {
+	 	if(tecla.which == 13)
+	 	{
+	 	$("#otroinput").focus();
+	 		//QUE SE POSICIONE EN OTRO INPUT
+	 	}
+	 	
+	 }
+	  // PREPARAR LOS EVENTOS
+	//DE TODOS MIS OBJETOS
+	 	$(".miboton").off("click",clicboton);
+	 	$(".miboton").on("click",clicboton2);
+	 	$("#uninput").on("keypress",teclauninput);
+
+	
+	
+	$("#miboton").off("click",clicboton);
 	// PREPARAR LOS EVENTOS
 	//DE TODOS MIS OBJETOS
 	$("#miboton").on("click",clicboton);
+
 }
 //MAIN
+//$(document).on("ready",inicio);
+
+
+	
+
+//MAIN
 $(document).on("ready",inicio);
+
